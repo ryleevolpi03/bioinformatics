5802ebfeb013ff0f95b64e8fb442d5c89d4e036f
ryleevolpi03 <rnh0854@wmich.edu>
2025-01-28 18:06:52 -0500

    learning_bash.sh

---
+++ bash_template.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+greeting="Hello"
+your_name=Rylee
+echo "$greeting $Rylee!”
+echo "This script is helping you learn bash."
+mkdir -p ~/Desktop/bioinformatics/bash_scripts
+mv ~/Desktop/bash_template.txt ~/Desktop/bioinformatics/bash_scripts/
+mv ~/Desktop/bioinformatics/bash_scripts/bash_template.txt ~/Desktop/bioinformatics/bash_scripts/bash_template.sh
+
